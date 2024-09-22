b6e054a230ed04022cd452cef33618c470ad7e08
Alex <alex.algarate@gmail.com>
2024-09-22 21:55:30 +0200

    header image mobile-desktop view

--- wedding_miguel_patri/views/header/components/image_header.py
+++ wedding_miguel_patri/views/header/components/image_header.py
@@ -11,15 +11,29 @@ def image_header() -> rx.Component:
         rx.Component: The header image component.
     """
 
-    return rx.image(
-        src=FileRoutes.IMAGE_HEADER.value,
-        alt=utils.alt_image_partners,
-        width="100%",
-        height="50%",
-        box_shadow="""
-                1px 1px 15px 10px rgba(0, 0, 0, 0.25)
-        """,
-        # box_shadow="0px 8px 5px 1px #F8F8FA inset",
-        align_self="stretch",
-        padding="-6px 12px",
+    return rx.vstack(
+        rx.mobile_only(
+            rx.image(
+                src=FileRoutes.IMAGE_HEADER.value,
+                alt=utils.alt_image_partners,
+                width="100%",
+                height="50%",
+                box_shadow="""
+                        1px 1px 14px 6px rgba(0, 0, 0, 0.25)
+                """,
+                align_self="stretch",
+                padding="-6px 12px",
+            ),
+        ),
+        rx.tablet_and_desktop(
+            rx.image(
+                src=FileRoutes.IMAGE_HEADER.value,
+                alt=utils.alt_image_partners,
+                width="100%",
+                height="50%",
+                box_shadow="0px 8px 5px 1px #F8F8FA inset",
+                align_self="stretch",
+                padding="-6px 12px",
+            ),
+        ),
     )
